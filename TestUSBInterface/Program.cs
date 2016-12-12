51560ccddd87b8cd75f75bbff3399f786e19c248
Artem Abramov <tematibr@gmail.com>
2016-12-12 21:59:08 +0300

    Add C# bindings to HIDAPI: USBInterface project
    Adds USBInterface lib which is a wrapper C# over HIDAPI
    Also adds TestUSBInterface project to test the wrapper.

---
+++ TestUSBInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USBInterface;
+
+namespace TestUSBInterface
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      USBDevice dev = new USBDevice();
+      dev.Open(1982, 23);
+      Console.WriteLine(dev.HIDisOpen);
+      Console.WriteLine(dev.Description());
+      dev.Close();
+      Console.ReadKey();
+    }
+  }
+}
